448db440153e87d30dc488c6e5eac75bb958a8ac
Andrew Randrianasulu <randrianasulu@gmail.com>
2022-04-03 02:51:56 +0300

    xmlto

---
+++ packages/xmlto/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://fedorahosted.org/xmlto
+TERMUX_PKG_DESCRIPTION="Converts xml to various document types"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.0.28
+TERMUX_PKG_REVISION=0
+TERMUX_PKG_SRCURL=http://releases.pagure.org/xmlto/xmlto-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2f986b7c9a0e9ac6728147668e776d405465284e13c74d4146c9cbc51fd8aad3 
+#TERMUX_PKG_DEPENDS="libxslt, openssl, libgcrypt, libgpg-error, libxml2"
